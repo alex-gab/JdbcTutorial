84f6111d8039ac5a5bfda8bad7d58ff543b25153
alex-gab <dragoi.alex@gmail.com>
2016-10-19 22:52:31 +0300

    [#1]: add a new department table to schema

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -2,4 +2,10 @@ CREATE TABLE Employee (
   name    VARCHAR,
   hiredate DATE,
   salary      NUMBER
-)
+);
+
+CREATE TABLE Department (
+  deptname    VARCHAR,
+  foundingdate DATE,
+  minimumsalary      NUMBER
+);
