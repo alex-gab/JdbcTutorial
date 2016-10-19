0a7c1a62dcc36df0574d18a5661f201835b7a8d2
alex-gab <dragoi.alex@gmail.com>
2016-10-19 08:22:57 +0300

    [#1]: integrate the new dbunit-builder-maven-plugin

--- src/test/java/com/alex/database/UserRepositoryImplTest.java
+++ src/test/java/com/alex/database/UserRepositoryImplTest.java
@@ -3,9 +3,10 @@ package com.alex.database;
 import org.dbunit.Assertion;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
-import org.dbunit.dataset.builder.annotations.CreateDataSetBuilders;
+import org.dbunit.dataset.builder.SchemaDataSetBuilder;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.h2.tools.RunScript;
@@ -14,11 +15,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.GregorianCalendar;
 
 import static com.alex.database.PropertiesHolder.*;
 import static java.nio.charset.Charset.forName;
+import static java.util.Calendar.DECEMBER;
+import static java.util.Calendar.JANUARY;
 
-@CreateDataSetBuilders
 public class UserRepositoryImplTest {
 
     private static final String JDBC_DRIVER = getDriver();
@@ -35,9 +38,7 @@ public class UserRepositoryImplTest {
 
     @Before
     public void importDataSet() throws Exception {
-        IDataSet dataSet = readDataSet();
-//        final IDataSet dataSet = readBuilderDataSet();
-//        final IDataSet dataSet = readGeneratedBuilderDataSet();
+        final IDataSet dataSet = readGeneratedBuilderDataSet();
         cleanlyInsert(dataSet);
     }
 
@@ -56,30 +57,14 @@ public class UserRepositoryImplTest {
         Assertion.assertEquals(expectedTable, actualTable);
     }
 
-    private IDataSet readDataSet() throws Exception {
-        return new FlatXmlDataSetBuilder().build(new File(getPath("/dataset.xml")));
+    private IDataSet readGeneratedBuilderDataSet() throws DataSetException {
+        final SchemaDataSetBuilder dataSetBuilder = new SchemaDataSetBuilder();
+        dataSetBuilder.newEMP_CLERKRow().ENAME("JAMES").HIREDATE(new GregorianCalendar(1981, DECEMBER, 3).getTime()).SAL(950).add().
+                newEMP_CLERKRow().ENAME("SMITH").HIREDATE(new GregorianCalendar(1980, DECEMBER, 17).getTime()).SAL(800).add().
+                newEMP_CLERKRow().ENAME("ADAMS").HIREDATE(new GregorianCalendar(1983, JANUARY, 12).getTime()).SAL(1100).add().
+                newEMP_CLERKRow().ENAME("MILLER").HIREDATE(new GregorianCalendar(1982, JANUARY, 23).getTime()).SAL(1900).add();
+        return dataSetBuilder.build();
     }
-
-//    private IDataSet readBuilderDataSet() throws DataSetException {
-//        ColumnSpec<String> ename = ColumnSpec.newColumn("ENAME");
-//        ColumnSpec<Date> hiredate = ColumnSpec.newColumn("HIREDATE");
-//        ColumnSpec<Integer> sal = ColumnSpec.newColumn("SAL");
-//        final DataSetBuilder builder = new DataSetBuilder();
-//        builder.newRow("EMP_CLERK").with(ename, "JAMES").with(hiredate, new GregorianCalendar(1981, DECEMBER, 3).getTime()).with(sal, 950).add();
-//        builder.newRow("EMP_CLERK").with(ename, "SMITH").with(hiredate, new GregorianCalendar(1980, DECEMBER, 17).getTime()).with(sal, 800).add();
-//        builder.newRow("EMP_CLERK").with(ename, "ADAMS").with(hiredate, new GregorianCalendar(1983, JANUARY, 12).getTime()).with(sal, 1100).add();
-//        builder.newRow("EMP_CLERK").with(ename, "MILLER").with(hiredate, new GregorianCalendar(1982, JANUARY, 23).getTime()).with(sal, 1900).add();
-//        return builder.build();
-//    }
-
-//    private IDataSet readGeneratedBuilderDataSet() throws DataSetException {
-//        final SchemaDataSetBuilder dataSetBuilder = new SchemaDataSetBuilder();
-//        dataSetBuilder.newEMP_CLERKRow().ENAME("JAMES").HIREDATE(new GregorianCalendar(1981, DECEMBER, 3).getTime()).SAL(950).add().
-//                newEMP_CLERKRow().ENAME("SMITH").HIREDATE(new GregorianCalendar(1980, DECEMBER, 17).getTime()).SAL(800).add().
-//                newEMP_CLERKRow().ENAME("ADAMS").HIREDATE(new GregorianCalendar(1983, JANUARY, 12).getTime()).SAL(1100).add().
-//                newEMP_CLERKRow().ENAME("MILLER").HIREDATE(new GregorianCalendar(1982, JANUARY, 23).getTime()).SAL(1900).add();
-//        return dataSetBuilder.build();
-//    }
 
     private void cleanlyInsert(IDataSet dataSet) throws Exception {
         databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
--- src/test/java/org/dbunit/dataset/builder/ISchemaDataSetBuilder.java
+++
@@ -1,8 +0,0 @@
-//package org.dbunit.dataset.builder;
-//
-//import org.dbunit.dataset.DataSetException;
-//import org.dbunit.dataset.IDataSet;
-//
-//interface ISchemaDataSetBuilder {
-//    IDataSet build() throws DataSetException;
-//}
